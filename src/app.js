5b55985b44aada1905009de6c00e66b6cfb90362
Jorge Leonardo Hernandez Rozo <seergee18@gmail.com>
2024-09-29 22:39:31 -0500

    adding env variable to frontend domain

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+require('dotenv').config()
 
 const auth_routes = require('./routes/auth_routes')
 const auth_admin_routes = require('./routes/auth_admin_routes')
@@ -16,7 +17,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:process.env.FRONTEND_ORIGIN,
     credentials: true // Permitir el envío de cookies o credenciales
 }))
 
