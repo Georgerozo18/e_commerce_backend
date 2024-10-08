2d390bbf53e393b068105c28fb04e8b90a4c7422
Jorge Leonardo Hernandez Rozo <seergee18@gmail.com>
2024-10-07 23:36:16 -0500

    update auth admin controller

--- src/controllers/auth_admin_controller.js
+++ src/controllers/auth_admin_controller.js
@@ -10,11 +10,7 @@ const admin_login_controller = async(request, response)=>{
             return response.status(400).json({message:'Username and password are required'})
         }
 
-        const admin = await User.findOne({
-            username, 
-            status:true, 
-            role:'admin'
-        })
+        const admin = await User.findOne({username, status:true, role:'admin'})
 
         if(!admin){
             return response.status(400).json({message:'Incorrect username or password'})
@@ -25,20 +21,46 @@ const admin_login_controller = async(request, response)=>{
             return response.status(400).json({message:'Incorrect username or password'})
         }
 
-        const token = jwt.sign({
+        // Generar el access token y el refresh token
+        const access_token = jwt.sign({
             id: admin._id,
             username: admin.username,
             fullname: admin.fullname,
-            role:admin.role
-        }, process.env.JWT_SECRET,{
-            expiresIn:'1h'
+            role: admin.role
+        }, process.env.JWT_SECRET, {
+            expiresIn: '15m'
         })
 
-        response.cookie('accessToken', token,{
-            httpOnly:true,
-            sameSite:'strict',
-            secure:process.env.NODE_ENV === 'production'
-        }).json({message:'Admin login successful'})
+        const refresh_token = jwt.sign({
+            id: admin._id,
+            username: admin.username,
+            fullname: admin.fullname,
+            role: admin.role
+        }, process.env.JWT_SECRET, {
+            expiresIn: '1h'
+        })
+
+        // Establecer las cookies
+        response.cookie('accessToken', access_token, {
+            httpOnly: true,
+            sameSite: 'strict',
+            secure: process.env.NODE_ENV === 'production'
+        })
+
+        response.cookie('refreshToken', refresh_token, {
+            httpOnly: true,
+            sameSite: 'strict',
+            secure: process.env.NODE_ENV === 'production'
+        })
+
+        response.status(200).json({
+            message: 'Admin login successful',
+            user: {
+                username: admin.username,
+                fullname: admin.fullname,
+                role: admin.role
+            }
+        })
 
     } catch(error){
         console.error(error)
